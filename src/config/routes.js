11ccdd8b2fe5e7fab4bc32df35d5bf2bf1bba91a
Raphael Denys <rdenys@gmail.com>
2024-07-18 13:43:18 -0300

    added auth and splitted APIs

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = function(server) {
+
+// PROTECTED ROUTES
+
+	const protectedApi = express.Router();
+	server.use('/api', protectedApi);
+
+	protectedApi.use(auth);
+
+	const BillingCycle = require('../api/billingCycle/billingCycleService');
+	BillingCycle.register(protectedApi, '/billingCycles');
+
+// OPEN ROUTES
+
+	const openApi = express.Router();
+	server.use('/oapi', openApi);
+
+	const AuthService = require('../api/user/authService');
+	openApi.post('/login', AuthService.login);
+	openApi.post('/signup', AuthService.signup);
+	openApi.post('/validateToken', AuthService.validateToken);
+
 	// define base url to routes
-	const router = express.Router();
-	server.use('/api', router);
+	// const router = express.Router();
+	// server.use('/api', router);
 
 	// map all billing cycle routes
-	const BillingCycle = require('../api/billingCycle/billingCycleService');
-	BillingCycle.register(router, '/billingCycles');
+	// const BillingCycle = require('../api/billingCycle/billingCycleService');
+	// BillingCycle.register(router, '/billingCycles');
 }
